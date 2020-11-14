d1bc6a38dbf580e30fea3af9b6a1fd5503acda13
Greg Balke <gbalke@berkeley.edu>
2020-11-13 22:49:39 -0800

    Progress continued

---
+++ firmware/current_monitor/src/main.c
@@ -0,0 +1,93 @@
+/**
+ * Primary executable for the current monitor chip.
+ */
+
+#include "stdint.h"
+#include "stdlib.h"
+
+#include "hw.h"
+#include "stm32l0xx_hal.h"
+#include "stm32l0xx_hal_gpio.h"
+#include "stm32l0xx_hal_rcc.h"
+
+/* Private variables ---------------------------------------------------------*/
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+
+int main(void) {
+  HAL_Init();
+
+  SystemClock_Config();
+
+  /* -2- Configure IO in output push-pull mode to drive external LEDs */
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+  GPIO_InitStruct.Pin = SHUNT_EN_PIN;
+  HAL_GPIO_Init(SHUNT_EN_PORT, &GPIO_InitStruct);
+
+  while(1) {
+    HAL_GPIO_WritePin(SHUNT_EN_PORT, SHUNT_EN_PIN, GPIO_PIN_SET);
+    HAL_Delay(0x4000);
+    HAL_GPIO_WritePin(SHUNT_EN_PORT, SHUNT_EN_PIN, GPIO_PIN_RESET);
+    HAL_Delay(0x4000);
+  }
+
+  return 1;
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = MSI
+  *            SYSCLK(Hz)                     = 2000000
+  *            HCLK(Hz)                       = 2000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 1
+  *            Flash Latency(WS)              = 0
+  *            Main regulator output voltage  = Scale3 mode
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  
+  /* Enable MSI Oscillator */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.MSICalibrationValue=0x00;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+  
+  /* Select MSI as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  
+}
